628f03cdf7f2c4a541eeb260f8e4f0e56ecefbba
MasayaKato00 <masaya.kato@recustomer.co>
2024-09-26 18:45:06 +0900

    silk追加

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
 
 from api.routers import routers
 
 urlpatterns = [
     path('api/', routers.api.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
